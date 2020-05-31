c8a77e4c175205e8cc4cf4644f8304e4821fc424
CaiyueDamowang <www.caiyue@qq.com>
2020-05-31 22:37:37 +0800

    reanem

---
+++ example-App/pages/index/index.js
@@ -0,0 +1,86 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+const page = Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    year: 2020,
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    // example
+
+    // const key = this.$get
+    // console.log(this.$fetch(['motto', 'id']))
+    // console.log(this.$fetch({
+    //   motto: (k)=>k,
+    //   id: (k) => k+1
+    // }))    
+    this.$fetch(['motto', 'id'])
+    this.$commit({ motto: 'weXagain' })
+    // this.$watch({
+    //   motto: ()=>{
+
+    //     let {year} = this.data
+    //     this.setData({
+    //       year: ++year
+    //     })
+    //   }
+    // })
+
+
+
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  tap(e) {
+    const data = this.data
+    let year = data.year
+
+    this.setData({
+      year: ++year
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
